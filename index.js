d0a2277d27f45e0c095ed0df53dbeab905b482ac
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-30 16:42:31 -0500

    App de prueba para subida de files a s3

--- index.js
+++ index.js
@@ -1,62 +1,45 @@
-const express = require("express");
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const { config } = require("dotenv");
-config();
-// Configurar la conexión a S3
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_BUCKET_REGION,
-});
+import express from "express";
+import fileUpload from "express-fileupload";
+import {
+  uploadFile,
+  getFiles,
+  getFile,
+  downloadFile,
+  getFileURL,
+} from "./s3.js";
 
-//const s3 = new aws.S3();
-
-// Configurar Multer para subir archivos a S3
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_BUCKET_NAME,
-    acl: "public-read",
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString() + file.originalname);
-    },
-  }),
-});
-
-// Iniciar la aplicación de Express
 const app = express();
 
-// Ruta para subir archivos
-app.post("/upload", upload.single("file"), (req, res) => {
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
+
+app.get("/files", async (req, res) => {
+  const result = await getFiles();
+  res.json(result.Contents);
+});
+
+app.get("/files/:fileName", async (req, res) => {
+  const result = await getFileURL(req.params.fileName);
   res.json({
-    url: req.file.location,
+    url: result,
   });
 });
 
-// Ruta para obtener la URL de un archivo
-app.get("/file/:fileName", (req, res) => {
-  const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: req.params.fileName,
-  };
-  s3.getSignedUrl("getObject", params, (err, url) => {
-    if (err) {
-      res.status(400).json({
-        error: "No se pudo obtener la URL del archivo",
-      });
-    } else {
-      res.json({
-        url: url,
-      });
-    }
-  });
+app.get("/downloadfile/:fileName", async (req, res) => {
+  await downloadFile(req.params.fileName);
+  res.json({ message: "archivo descargado" });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor iniciado en el puerto 3000");
+app.post("/files", async (req, res) => {
+  const result = await uploadFile(req.files.file);
+  res.json({ result });
 });
+
+app.use(express.static("images"));
+
+app.listen(3000);
+console.log(`Server on port ${3000}`);
